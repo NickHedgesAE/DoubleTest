3239dd4ce010652c5c06729537dba081ffd7e87a
NickHedgesAE <91309219+NickHedgesAE@users.noreply.github.com>
2021-10-29 09:23:31 +0200

    Add files via upload

---
+++ Befimmo/GoogleTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Befimmo
+{
+    public class OtherTests
+    {
+        private IWebDriver driver;
+        private IJavaScriptExecutor js;
+        public IDictionary<string, object> vars { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+
+        [Test]
+        public void Events()
+        {
+            driver.Navigate().GoToUrl("https://www.google.be");
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+    
+}
